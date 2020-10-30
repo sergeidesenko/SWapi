d8936d6e2b03f6354ab477f5d8c927695ef670e6
Sergey Desenko <sergeydesenko@gmail.com>
2020-10-30 16:11:59 +0300

    Initial Commit

---
+++ SWapi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SWapi
+//
+//  Created by user on 30.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
